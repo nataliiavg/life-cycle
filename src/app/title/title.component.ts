84854e8ebd93c8b0135552f048c686c55c4ba0ea
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-04 15:22:56 -0300

    Understanding OnInit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-title',
-  templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+	selector: 'app-title',
+	templateUrl: './title.component.html',
+	styleUrls: ['./title.component.css'],
 })
 export class TitleComponent implements OnInit {
+	nome: string = 'Natalia';
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.nome = `Olá, ${this.nome}`;
+	}
 }
