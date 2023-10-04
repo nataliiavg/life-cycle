1e3311b157c11798efe4b07920fe6046adb913ad
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-04 15:44:32 -0300

    Add hook OnChanges

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-title',
 	templateUrl: './title.component.html',
 	styleUrls: ['./title.component.css'],
 })
-export class TitleComponent implements OnInit {
-	nome: string = 'Natalia';
-	constructor() {}
+export class TitleComponent implements OnInit, OnChanges {
+	// modifica o valor a partir do input
+	@Input() nome: string = '';
+
+	constructor() {
+		console.log(`Construtor ${this.nome}`);
+	}
+	ngOnChanges(): void {
+		console.log(`OnChanges ${this.nome}`);
+	}
 
 	ngOnInit(): void {
-		this.nome = `Olá, ${this.nome}`;
+		this.nome = this.nome + "X"
+		console.log(`OnInit ${this.nome}`);
 	}
 }
