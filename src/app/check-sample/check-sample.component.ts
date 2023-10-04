c1d9ef48192146e380f0fa64795f7ab371ecfb44
Natalia Graciano <nataliavgraciano@gmail.com>
2023-10-04 17:14:34 -0300

    Add order DoCheck, content and view

--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -25,26 +25,35 @@ export class CheckSampleComponent
 	quantidade: number = 0;
 	constructor() {}
 
-	adicionar(){
+	adicionar() {
 		this.quantidade += 1;
 	}
 
-	decrementar(){
-	this.quantidade -= 1;
-}
+	decrementar() {
+		this.quantidade -= 1;
+	}
+	// Checked -> Content -> View
 
-	ngAfterContentChecked(): void {
-		console.log('ngAfterContentChecked');
-	}
-	ngAfterViewChecked(): void {
-		console.log('ngAfterViewChecked');
-	}
-	ngAfterViewInit(): void {
-		console.log('ngAfterViewInit');
-	}
+	// Quando o primeiro conteúdo é iniciado
 	ngAfterContentInit(): void {
 		console.log('ngAfterContentInit');
 	}
+
+	// Depois da inicialização da view
+	ngAfterViewInit(): void {
+		console.log('ngAfterViewInit');
+	}
+
+	// Após alguma alteração, verifica o conteúdo
+	ngAfterContentChecked(): void {
+		console.log('ngAfterContentChecked');
+	}
+
+	// após alguma alteração, verifica a view
+	ngAfterViewChecked(): void {
+		console.log('ngAfterViewChecked');
+	}
+
 	ngDoCheck(): void {
 		console.log('ngDoCheck');
 	}
